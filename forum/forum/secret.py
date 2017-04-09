05c6cf52a2841a97c45d68d3dc565f81e2900390
xiao_lingran@163.com <root@localhost.localdomain>
2017-04-09 17:23:48 +0800

    20170524

--- forum/forum/secret.py
+++ forum/forum/secret.py
@@ -1,8 +1,8 @@
 from django.core.mail import send_mail
 EMAIL_USE_SSL = True
-EMAIL_HOST = "smtp.qq.com"
+EMAIL_HOST = 'smtp.163.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER="670399751@qq.com"
-EMAIL_HOST_PASSWORD="yxnyyyuipzqabefd"
-EMAIL_FORM_EMAIL="670399751@qq.com"
+EMAIL_HOST_USER = 'xiao_lingran@163.com'
+EMAIL_HOST_PASSWORD = 'jcszjswk123'
+DEFAULT_FROM_EMAIL = 'xiao_lingran@163.com'
 
